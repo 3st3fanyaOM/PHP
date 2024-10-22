bdc3d0053322ca060ca19ade6ba6fa849699cb0a
3st3fayaOM <eortega8634@ceuandalucia.es>
2024-10-22 16:08:35 +0200

    2210

--- aqui_no_vendemos_cruzcampo/registro.php
+++ aqui_no_vendemos_cruzcampo/registro.php
@@ -7,13 +7,49 @@
 </head>
 <body>
     <?php
-    $errores="";
 
+$erroresSubidaArchivo = array(
+    0 => 'El archivo de ha subido correctamente',
+    1 => 'Excede el tamaño máximo del sistema',
+    2 => 'Excede el tamaño máximo especificado',
+    3 => 'El archivo no se ha subido completamente',
+    4 => 'No se ha subido el archivo',
+    6 => 'La carpeta temporal no existe',
+    7 => 'Fallo al escribir en el disco',
+    8 => 'Una extensión PHP ha detenido la descarga',
+);
+
+if($_FILES["fichero"]["error"] > 0)
+{
+echo "Error: " . $msgError[$_FILES["fichero"]["error"]] . "<br />";
+}
+else
+{
+echo "Nombre original: " . $_FILES["fichero"]["name"] . "<br />";
+echo "Tipo: " . $_FILES["fichero"]["type"] . "<br />";
+echo "Tamaño: " . ceil($_FILES["fichero"]["size"] / 1024) . " Kb<br />";
+echo "Nombre temporal: " . $_FILES["fichero"]["tmp_name"] . "<br />";
+if(file_exists("upload/" . $_FILES["fichero"]["name"]))
+{
+echo $_FILES["fichero"]["name"] . " ya existe";
+}
+else
+{
+move_uploaded_file($_FILES["fichero"]["tmp_name"],
+"upload/" . $_FILES["fichero"]["name"]);
+ 
+echo "Almacenado en: " . "upload/" . $_FILES["fichero"]["name"];
+}
+}
+
+
+    $errores="";
+    error_reporting(0);
     if (trim($_REQUEST['marca'])=="")
         $errores=$errores."<li>Se requiere marca</li>";
     if (trim($_REQUEST['advertencia'])=="")
         $errores=$errores."<li>Se requiere la advertencia sobre el abuso del consumo de alcohol</li>";
-    if (trim($_REQUEST['fecha'])=="")
+    if (trim($_REQUEST['fechacaducidad'])=="")
         $errores=$errores."<li>Se requiere fecha</li>";
     if (!$_REQUEST['alergenos'])
         $errores=$errores."<li>Es obligatorio incluir alérgenos</li>";
@@ -30,7 +66,10 @@
         echo "Cantidad: ".$_REQUEST['cantidad'].'<br>';
         echo "Marca: ".$_REQUEST['marca'].'<br>';
         echo "Advertencia: ".$_REQUEST['advertencia'].'<br>';
-        echo "Fecha caducidad: ".$_REQUEST['fecha'].'<br>';
+        echo "Fecha caducidad: ".$_REQUEST['fechacaducidad'].'<br>';
+        foreach ($alergenos as $alergeno){
+            echo $alergeno." ";
+        }
         echo "Alérgenos: ".$_REQUEST['alergenos'].'<br>';
         echo "Observaciones: ".$_REQUEST['observaciones'].'<br>';
     }
